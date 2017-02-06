4cc6f80f48aa3f1c26cf11be34b6a6353b7e7dc4
VICTORocv <gwqocv@icloud.con>
2017-02-06 10:54:40 +0800

    Initial Commit

---
+++ same-tree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  same-tree
+//
+//  Created by 郭悟清 on 2017/2/6.
+//  Copyright © 2017年 郭悟清. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
