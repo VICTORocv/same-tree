276921d1cbceb455d8883b47c77232b7f3da2730
VICTORocv <gwqocv@icloud.con>
2017-02-06 12:03:14 +0800

    same-tree from leetcode

--- same-tree/main.cpp
+++ same-tree/main.cpp
@@ -7,9 +7,40 @@
 //
 
 #include <iostream>
-
+struct TreeNode
+{
+   int      val;
+   TreeNode *left;
+   TreeNode *right;
+   TreeNode(int x): val(x),left(NULL),right(NULL){}
+};
+class Solution
+{
+public:
+    bool isSameTree(TreeNode *p,TreeNode *q)
+    {
+        if(p&&q)
+        {
+            if(p->val==q->val)
+            {
+                return isSameTree(p->left, q->left)&&isSameTree(p->right, q->right);
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else if (!p&&!q)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
     return 0;
 }
